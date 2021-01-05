d138c495ad37ea478b656f2b50edbdde5ecee615
myfairy56 <myfairy56@naver.com>
2021-01-06 01:17:41 +0900

    commit

--- 09_parameter.py
+++ 09_parameter.py
@@ -1,6 +1,6 @@
-def hello(name):
-    print("Hello!")
-    print(name)
-    print("Welcome to Codeit!")
+#한 함수에 여러 개의 파라미터를 동시에 전달
+def print_sum(a, b, c) #print_sum이라는 함수를 생성하고 a, b라는 2개의 파라미터를 생성
+    print(a + b + c)
 
-hello("Tom")
+print_sum(7, 3, 2) #7이 a에 지정 3이 b에 지정
+#파라미터 이름은 자유롭게 지정 가능 > num_1, num_2, num_3...
