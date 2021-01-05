47fe550abb5f7eeccd11efa0c492fdaed8dd3b3e
myfairy56 <myfairy56@naver.com>
2021-01-06 01:10:27 +0900

    commit

---
+++ 08_parameter.py
@@ -0,0 +1,9 @@
+def hello(name):
+    print("Hello!")
+    print(name)
+    print("Welcome to Codeit!")
+
+hello("Tom")
+#함수를 호출할 때마다 다르게 동작하도록 하고 싶다.
+#hello 함수를 호출할 때 문자열을 다르게 호출
+#이때 괄호 안에 있는 것이 파라미터
---
+++ hello.py
@@ -0,0 +1,3 @@
+print('Hello Codeit!')
+print(2+5)
+print(2*5)
